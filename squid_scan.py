5ea4191b9b87d60374d048ee5e455a8856af6831
vsamiamv <vsamiamv@outlook.com>
2021-01-08 23:48:57 -0500

    Update squid_scan.py

--- squid_scan.py
+++ squid_scan.py
@@ -20,9 +20,9 @@ def set_proxy(creds, proxy):
     return proxies
 
 def scan_proxy(target):
-    common_ports = {21,22,23,25,53,69,80,109,110,123,137,138,139,143,156,389,443,546,547,995,993,2086,2087,2082,2083,3306,8080,8443,9000,9090,10000}
+    ports = {21,22,23,25,53,69,80,109,110,123,137,138,139,143,156,389,443,546,547,995,993,2086,2087,2082,2083,3306,8080,8443,9000,9090,10000}
     proxies = set_proxy(options.creds, options.proxy)
-    for i in sorted(common_ports):
+    for i in sorted(ports):
         port = str(i)
         r = requests.get("http://" + target + ":" + port, proxies=proxies)
         if r.status_code == 200:
